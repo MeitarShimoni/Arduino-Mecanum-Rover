3e45f8bb558e741893735db33e64489b302cc416
Meitar Shimoni <141142200+MeitarShimoni@users.noreply.github.com>
2025-04-19 13:11:05 +0300

    Update Rover_control.ino

--- Rover_control.ino
+++ Rover_control.ino
@@ -1,34 +1,113 @@
-void moveForward() {
-  digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
+
+
+// Functions Of the Rover Here:
+
+void SetMotors(){
+  // motors set up maybe later it wont be constant
+  motor1.setSpeed(speedpwm);  //front left
+  motor2.setSpeed(speedpwm);  // back left
+  motor3.setSpeed(speedpwm);  // back right
+  motor4.setSpeed(speedpwm);  // front right
+  // Servos setup
+  Serial.println("Motors Set!");
 }
 
-void moveBackward() {
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
+void Forward(int duration) {
+  Serial.println("Driving Forward ");
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  delay(duration);
 }
 
-void turnLeft() {
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
+void Backward(int duration) {
+  Serial.println("Driving Backward ");
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+  delay(duration);
 }
 
-void turnRight() {
-  digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
+void Stop() {
+  Serial.println("Motors Stopped ");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
 }
 
-void stopMotors() {
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
+
+void turn360CCW(int time180) {
+  Serial.println("Turning 360 degrees ");
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+  delay(time180 * 2);
+  Stop();
+}
+
+void turn180CCW(int time180) {
+  Serial.println("Turning 180 degrees ");
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+  delay(time180);
+  if (time180 > 0){
+    Stop();
+  }
+}
+
+void turn180CW(int time180) {
+  Serial.println("Turning 180 degrees ");
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  delay(time180);
+  if (time180 > 0){
+    Stop();
+  }
+  
+}
+
+void MoveLeft(int duration){
+  Serial.println("Driving Moving Left ");
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
+  delay(duration);
+}
+
+void MoveRight(int duration){
+  Serial.println("Driving Moving Left ");
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);
+  delay(duration);
+}
+
+void GetExcited(int Duration){
+  Serial.println("Rover Excited!");
+  turn180CW(Duration);
+  turn180CCW(Duration);
+  turn180CW(Duration);
+  turn180CCW(Duration);
+  turn180CW(Duration);
+  turn180CCW(Duration);
+  Stop();
+}
+
+void GoSleep(){
+
+
+}
+
+void WakeUp(){
+
 }
